66e58ae9cfd4f61d1bf3911453e8798dee1a8f9d
Drew Eiswerth <dreweiswerth@gmail.com>
2022-02-15 17:05:35 -0500

    minor correction

--- src/components/SortingVisualizer/SortingVisualizer.js
+++ src/components/SortingVisualizer/SortingVisualizer.js
@@ -64,7 +64,7 @@ export function SortingVisualizer() {
     }
 
     function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     function performAnimations(algorithm, color, speed) {
